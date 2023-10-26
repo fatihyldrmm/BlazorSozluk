7a12d22018a7a6a4d994f4d6f08db523cd1c0793
Fatih Yıldırım <fatihyldrm3693@gmail.com>
2023-10-26 12:28:38 +0300

    Add project files.

---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/EntryCommentFavorite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class EntryCommentFavorite : BaseEntity
+    {
+        public Guid EntryCommentId { get; set; }
+        public Guid CreatedById { get; set; }
+
+
+        public EntryComment EntryComment { get; set; }
+        public User CreatedUser { get; set; }
+    }
+}
---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/EntryFavorite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class EntryFavorite : BaseEntity
+    {
+        public Guid EntryId { get; set; }
+        public Guid CreatedById { get; set; }
+
+
+        public Entry Entry { get; set; }
+        public User CreatedUser { get; set; }
+    }
+}
